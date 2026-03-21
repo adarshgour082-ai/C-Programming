63ded271ddd094c1e8f23fddbe68d89e794ae44c
Adarsh Gour <adarshgour082@gmail.com>
2026-03-22 00:48:50 +0530

    Add initial C program that prints greetings

---
+++ Assignments/index.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    printf("//  Adarsh Gour  // \n");
+    printf("Hello World \n");
+    printf("Hello World \n");
+    printf("//  Adarsh Gour  // ");
+
+   return 0;
+}
+
